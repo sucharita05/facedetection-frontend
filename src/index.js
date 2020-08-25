1704f84ecd2df240e4ef352bbe8f876891d62f16
sucharita05 <68145021+sucharita05@users.noreply.github.com>
2020-08-25 21:13:25 +0530

    uploade in website

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.render(<App />,
+  // <React.StrictMode>
+  // </React.StrictMode>,
   document.getElementById('root')
 );
 
